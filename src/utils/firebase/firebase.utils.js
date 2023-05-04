dcc6acf969af1af425e39835b4ae73f4b52c86ff
ts <timors23@protonmail.com>
2023-05-04 20:03:42 +0300

    squash! setting up signUP form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -24,13 +24,16 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({
     prompt: 'select_account'
 })
 
 export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () =>
+    signInWithPopup(auth, googleProvider)
+export const signInWithGoogleRedirect = () =>
+    signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore()
 
